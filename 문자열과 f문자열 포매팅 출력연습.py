53b23b7080d5c4ef2974057ebaf941989c895ba6
ley <bojogeyoung@naver.com>
2024-06-05 16:29:38 +0900

    수정

--- 문자열과 f문자열 포매팅 출력연습.py
+++ 문자열과 f문자열 포매팅 출력연습.py
@@ -3,5 +3,5 @@ order = input("==> 어떤 음료를 주문하시겠어요?")
 number = int(input("==> 몇 잔 드릴까요?"))
 
 print() #한 줄 띄어쓰기
-print("==> &s님은 %s를 %d잔 주문하셨습니다." %(name,order,number))
+print("==> %s님은 %s를 %d잔 주문하셨습니다." %(name,order,number))
 print(f"==> {name}님은 {order}를 {number}잔 주문하셨습니다.")    
